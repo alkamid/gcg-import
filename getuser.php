0de0533803c5cf886b691ae93b3d8127eddf1fd9
alkamid <adamkli@gmail.com>
2016-03-13 19:23:30 +0000

    first commit

---
+++ getuser.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+$q = intval($_GET['q']);
+
+include "config.php";
+   
+$con = mysqli_connect('localhost','root', $mysqlpwd, $mysqldbname);
+
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+
+mysqli_select_db($con,"scrabble");
+
+$sql="SELECT PFSTOURS.name, PFSTOURHH.runda, PFSTOURHH.result1, PFSPLAYER.name_show, PFSTOURHH.result2, PFSTOURHH.host
+      FROM PFSTOURHH
+      JOIN PFSTOURS
+      ON PFSTOURHH.turniej=PFSTOURS.id
+      JOIN PFSPLAYER
+      ON PFSTOURHH.player2 = PFSPLAYER.id
+      WHERE player1 = '".$q."'
+      ORDER BY PFSTOURS.name, PFSTOURHH.runda";
+
+$result = mysqli_query($con,$sql);
+
+echo "<table>
+<tr>
+<th>Turniej</th>
+<th>Runda</th>
+<th>Gospodarz</th>
+<th>Gość</th>
+<th>Wynik 1</th>
+<th>Wynik 2</th>
+</tr>";
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['runda'] . "</td>";
+   if ($row['host'] == 1) {
+   $order = array($q, $row['name_show'], $row['result1'], $row['result2']);
+   }
+   else {
+   $order = array($row['name_show'], $q, $row['result2'], $row['result1']);
+   }
+    echo "<td>" . $order[0] . "</td>";
+    echo "<td>" . $order[1] . "</td>";
+    echo "<td>" . $order[2] . "</td>";
+    echo "<td>" . $order[3] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+mysqli_close($con);
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<script>
+function showUser(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("txtHint").innerHTML = xmlhttp.responseText;
+            }
+        };
+        xmlhttp.open("GET","getuser.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+</script>
+</head>
+<body>
+
+<form>
+<select name="users" onchange="showUser(this.value)">
+  <option value="">Select a person:</option>
+<?php
+
+include 'config.php';
+
+$con = mysqli_connect('localhost','root', $mysqlpwd, $mysqldbname);
+if (!$con) {
+    die('Could not connect: ' . mysqli_error($con));
+}
+mysql_set_charset('utf8mb4', $con);
+$sql="SELECT * FROM PFSPLAYER";
+$result = mysqli_query($con,$sql);
+while($row = mysqli_fetch_array($result)) {
+    echo "<option value='". $row['id'] . "'>" . $row['name_alph'] . "</option>";
+}
+?>
+  </select>
+</form>
+<br>
+<div id="txtHint"><b>Person info will be listed here...</b></div>
+
+</body>
+</html>
