3a1b8e9180dd516991b828cad6f72fdc932be8bd
alkamid <adamkli@gmail.com>
2016-04-04 08:32:53 +0100

    +

--- download.php
+++ download.php
@@ -2,6 +2,7 @@
 header('Content-type: text/plain');
 header('Content-Disposition: attachment; filename="zapis.gcg"');
 include 'config.php';
+mb_internal_encoding("UTF-8");
 $con = mysqli_connect($mysqlhost,$mysqluser, $mysqlpwd, $mysqldbname);
 $query = 'SELECT gcg FROM PFSTOURHH WHERE turniej = ' . $_GET['turniej'] . ' AND runda = ' . $_GET['runda'] . ' AND player1 = ' . $_GET['p1'] . ';';
 mysqli_set_charset($con, 'utf8');
